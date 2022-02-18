196103e6600553281ee39d53ac969435cc8fdbf4
jacobjolibois <jacobjolibois@hey.com>
2022-02-18 09:34:28 -0600

    fixed bug with migration

--- data/migrations/20220218140416_tables.js
+++ data/migrations/20220218140416_tables.js
@@ -3,18 +3,18 @@ exports.up = function(knex, Promise) {
   return knex.schema
 		.createTable('projects', tbl => {
 			tbl.increments('project_id');
-			tbl.string('project_name', 128).nonNullable();
+			tbl.string('project_name', 128).notNullable();
 			tbl.string('project_description', 256);
 			tbl.boolean('project_completed');
 		})
 		.createTable('tasks', tbl => {
 			tbl.increments('task_id');
-			tbl.string('task_description', 128).nonNullable();
+			tbl.string('task_description', 128).notNullable();
 			tbl.string('task_notes', 256);
 			tbl.boolean('task_completed');
 			tbl.integer('project_id')
 				.unsigned()
-				.nonNullable()
+				.notNullable()
 				.references('project_id')
 				.inTable('projects');
 		})
@@ -26,15 +26,15 @@ exports.up = function(knex, Promise) {
 		.createTable('project_resources', tbl => {
 			tbl.integer('project_id')
 				.unsigned()
-				.nonNullable()
+				.notNullable()
 				.references('project_id')
 				.inTable('projects');
 			tbl.integer('resource_id')
 				.unsigned()
-				.nonNullable()
+				.notNullable()
 				.references('resource_id')
 				.inTable('resources');
-			tbl.string('quantity', 128).nonNullable();
+			tbl.string('quantity', 128).notNullable();
 			tbl.primary(['project_id', 'resource_id']);
 		});
 };
