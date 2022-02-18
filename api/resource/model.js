c3a0c35c72971f84f8caebd96cdc1bd5c053bc00
jacobjolibois <jacobjolibois@hey.com>
2022-02-18 12:44:06 -0600

    fixed another bug with formatting the resource returned object

--- api/resource/model.js
+++ api/resource/model.js
@@ -2,11 +2,13 @@ const db = require('../../data/dbConfig');
 
 // Models
 async function addNew(resource) {
-	const sent = await db('resources').insert(resource);
+	const sentResource = await db('resources').insert(resource);
 	
-	const newResource = await db('resources').where('resource_id', sent[0]);
-	console.log(newResource);
-	return newResource;
+	const newResource = await db('resources').where('resource_id', sentResource[0]);
+	
+	return {
+		resource_name: newResource[0].resource_name
+	};
 }
 
 function getAll() {
